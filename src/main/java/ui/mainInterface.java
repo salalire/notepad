5538580569dcbc85eaa3fe0ad13d138b706b568d
Salah <samuellire076@gmail.com>
2026-04-26 22:28:21 +0300

    add items in the menu

--- src/main/java/ui/mainInterface.java
+++ src/main/java/ui/mainInterface.java
@@ -2,6 +2,7 @@ package ui;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -11,6 +12,40 @@ public class mainInterface {
         Menu fileMenu=new Menu("File");
         Menu editMenu= new Menu("Edit");
         Menu viewMenu =new Menu("View");
+        fileMenu.getItems().addAll(
+                new MenuItem("New Tab"),
+                new MenuItem("New Window"),
+                new MenuItem("Open"),
+                new MenuItem("recent"),
+                new MenuItem("save"),
+                new MenuItem("save as"),
+                new MenuItem("save all"),
+                new MenuItem("close tab"),
+                new MenuItem("close window"),
+                new MenuItem("exit")
+        );
+
+        editMenu.getItems().addAll(
+                new MenuItem("Undo"),
+                new MenuItem("Copy"),
+                new MenuItem("Cut"),
+                new MenuItem("Paste"),
+                new MenuItem("Delete"),
+                new MenuItem("Find"),
+                new MenuItem("Find next"),
+                new MenuItem("Find previous"),
+                new MenuItem("Replace"),
+                new MenuItem("Go to"),
+                new MenuItem("Select all"),
+                new MenuItem("Time/date"),
+                new MenuItem("Font")
+        );
+        viewMenu.getItems().addAll(
+                new MenuItem("Zoom"),
+                new MenuItem("Status Bar"),
+                new MenuItem("Word wrap")
+
+        );
         MenuBar menuBar=new MenuBar(fileMenu,editMenu,viewMenu);
         TextArea area=new TextArea();
         BorderPane borderPane=new BorderPane();
